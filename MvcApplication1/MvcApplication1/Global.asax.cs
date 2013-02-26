66b468c0feba7b5870f663e60689dd3c9f1ee51c
Henrique <Guerra>
2013-02-26 09:47:53 +0000

    another commit

--- MvcApplication1/MvcApplication1/Global.asax.cs
+++ MvcApplication1/MvcApplication1/Global.asax.cs
@@ -31,8 +31,6 @@ namespace MvcApplication1
 
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
         }
